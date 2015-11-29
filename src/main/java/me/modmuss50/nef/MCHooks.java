af5910ddd2ee2074c9de57a6c784ff2157ee15ce
Modmuss50 <modmuss50@gmail.com>
2015-11-29 12:31:57 +0000

    Some small tweaks

--- src/main/java/me/modmuss50/nef/MCHooks.java
+++ src/main/java/me/modmuss50/nef/MCHooks.java
@@ -6,7 +6,9 @@ public class MCHooks {
 
     public static void checkNull(Item item) {
         if (item == null) {
-            throw new NullPointerException("An itemstack was created with a null item!");
+          //  throw new NullPointerException("An itemstack was created with a null item!");
+            System.out.println("An itemstack was created with a null item!");
+            Thread.dumpStack();
         }
     }
 }
